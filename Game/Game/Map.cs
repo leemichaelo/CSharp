b0f3e0f6d1e526870f2e6810a7fde66065f3ad8c
Lee Owen <LeeMichaelO@outlook.com>
2018-05-26 13:32:43 -0700

    TreehouseDefense

---
+++ Game/Game/Map.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreehouseDefense
+{
+    class Map
+    {
+        //--**Attributes**--
+        //readonly makes it so that they can't change the attribute
+        public readonly int Width;
+        public readonly int Height;
+
+        //--**Behaviors**--
+        //Constructor
+        public Map(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        //Checks if the point is within bounds of the map
+        public bool OnMap(Point point)
+        { 
+            return point.X >= 0 && point.X < Width && 
+                   point.Y >= 0 && point.Y < Height;
+        }
+    }
+}
