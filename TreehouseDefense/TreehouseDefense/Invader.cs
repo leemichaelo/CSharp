643f22e7e848bc363adfdfc34855ec1ca4b6eef6
Lee Owen <LeeMichaelO@outlook.com>
2018-05-27 20:43:14 -0700

    Expression Bodied Members

--- TreehouseDefense/TreehouseDefense/Invader.cs
+++ TreehouseDefense/TreehouseDefense/Invader.cs
@@ -11,22 +11,11 @@ namespace TreehouseDefense
         private readonly Path _path;
         private int _pathStep = 0;
 
-        public MapLocation Location
-        {
-            get
-            {
-                return _path.GetLocationAt(_pathStep);
-            }
-        }
+        public MapLocation Location => _path.GetLocationAt(_pathStep);
 
-        public Invader(Path path)
-        {
-            _path = path;
-        }
+        public Invader(Path path) => _path = path;
 
-        public void Move()
-        {
-            _pathStep += 1;
-        }
+        public void Move() => _pathStep += 1;
+
     }
 }
