8c6fb8e2905601350882ab90455c58deb04324fa
Lee Owen <LeeMichaelO@outlook.com>
2018-05-27 20:29:49 -0700

    Using Properties

--- TreehouseDefense/TreehouseDefense/Invader.cs
+++ TreehouseDefense/TreehouseDefense/Invader.cs
@@ -8,6 +8,20 @@ namespace TreehouseDefense
 {
     class Invader
     {
+        private readonly Path _path;
+        private int _pathStep = 0;
         public MapLocation Location { get; private set; }
+
+        public Invader(Path path)
+        {
+            Location = path.GetLocationAt(_pathStep);
+            _path = path;
+        }
+
+        public void Move()
+        {
+            _pathStep += 1;
+            Location = _path.GetLocationAt(_pathStep);
+        }
     }
 }
