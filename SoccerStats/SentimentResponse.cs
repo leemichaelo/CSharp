91c337e7479379e728cd8fbfec163a7da1bff8b6
Lee Owen <LeeMichaelO@outlook.com>
2018-06-30 09:31:17 -0700

    Working with Microsoft Text Analytics API

---
+++ SoccerStats/SentimentResponse.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoccerStats
+{
+
+    public class SentimentResponse
+    {
+        [JsonProperty(PropertyName = "documents")]
+        public List<Sentiment> Sentiments { get; set; }
+
+        [JsonProperty(PropertyName = "errors")]
+        public List<Error> Errors { get; set; }
+    }
+
+    public class Sentiment
+    {
+        [JsonProperty(PropertyName = "score")]
+        public float Score { get; set; }
+
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+    }
+
+    public class Error
+    {
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+
+        [JsonProperty(PropertyName = "message")]
+        public string Message { get; set; }
+    }
+
+}
