a83f75d9a15de489a6986516db6c25b44e1de6e8
Lee Owen <LeeMichaelO@outlook.com>
2018-06-30 18:02:04 -0700

    Working with Jagged and Multidimensional Arrays

---
+++ CSharpCollections/Cell.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpCollections
+{
+    class Cell
+    {
+        public string Contents { get; set; }
+    }
+}
---
+++ CSharpCollections/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Print Jaggaed Array
+            int[][] jaggedArray = BuildJaggedMultiplicationTable(3);
+
+            foreach (int[] row in jaggedArray)//Print the array
+            {
+                foreach (int cell in row)
+                {
+                    System.Console.Write(cell);
+                }
+                System.Console.WriteLine();
+            }
+
+            //Print Multidimensional Array
+            int[,] multiSheet = BuildMultidimensionalMultiplicationTable(3);
+            for (int i = 0; i < multiSheet.GetLength(0); i++)
+            {
+                for (int j = 0; j < multiSheet.GetLength(1); j++)
+                {
+                    Console.Write(multiSheet[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        //Build Jagged Array
+        public static int[][] BuildJaggedMultiplicationTable(int maxFactor)
+        {
+            int[][] arrayToReturn = new int[maxFactor + 1][];
+            for (int i = 0; i < arrayToReturn.Length; i++)
+            {
+                arrayToReturn[i] = new int[maxFactor + 1];
+                for (int j = 0; j < arrayToReturn[i].Length; j++)
+                {
+                    arrayToReturn[i][j] = i * j;
+                }
+            }
+            return arrayToReturn;
+        }
+
+        public static int[,] BuildMultidimensionalMultiplicationTable(int maxFactor)
+        {
+            int[,] arrayToReturn = new int[maxFactor + 1, maxFactor + 1];
+            for (int i = 0; i < arrayToReturn.GetLength(0); i++)
+            {
+                for (int j = 0; j < arrayToReturn.GetLength(1); j++)
+                {
+                    arrayToReturn[i, j] = i * j;
+                }
+            }
+            return arrayToReturn;
+        }
+    }
+}
