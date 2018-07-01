288546491ce1cf31feeaefb0fbde0fb11097659a
Lee Owen <LeeMichaelO@outlook.com>
2018-07-01 13:40:43 -0700

    Working with collection interfaces

---
+++ CSharpCollections/SchoolRoll.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpCollections
+{
+    class SchoolRoll
+    {
+        private List<Student> _students = new List<Student>();
+
+        //By passing in "Ienumberable", it allows all collections to be passed in since all collections inherit from ienumberable
+        public void AddStudents(IEnumerable<Student> students)
+        {
+            _students.AddRange(students);
+        }
+    }
+}
