7be731c1d52378a8b2753d29ac582ac8eda18af3
Lee Owen <LeeMichaelO@outlook.com>
2018-07-01 09:45:25 -0700

    Comparing Lists

--- CSharpCollections/Program.cs
+++ CSharpCollections/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CSharpCollections
 {
@@ -10,85 +7,19 @@ namespace CSharpCollections
     {
         static void Main(string[] args)
         {
-            //Print Jaggaed Array
-            int[][] jaggedArray = BuildJaggedMultiplicationTable(3);
-
-            foreach (int[] row in jaggedArray)//Print the array
+            List<Student> students = new List<Student>
             {
-                foreach (int cell in row)
-                {
-                    System.Console.Write(cell);
-                }
-                System.Console.WriteLine();
-            }
+                new Student() { Name = "Sally", GradeLevel = 3 },
+                new Student() { Name = "Bob", GradeLevel = 3 },
+                new Student() { Name = "Sally", GradeLevel = 2 }
+            };
 
-            //Print Multidimensional Array
-            int[,] multiSheet = BuildMultidimensionalMultiplicationTable(3);
-            for (int i = 0; i < multiSheet.GetLength(0); i++)
+            students.Sort();
+
+            foreach(Student student in students)
             {
-                for (int j = 0; j < multiSheet.GetLength(1); j++)
-                {
-                    Console.Write(multiSheet[i, j]);
-                }
-                Console.WriteLine();
+                Console.WriteLine($"{student.Name} is in grade {student.GradeLevel}");
             }
         }
-
-        //Build Jagged Array
-        public static int[][] BuildJaggedMultiplicationTable(int maxFactor)
-        {
-            int[][] arrayToReturn = new int[maxFactor + 1][];
-            for (int i = 0; i < arrayToReturn.Length; i++)
-            {
-                arrayToReturn[i] = new int[maxFactor + 1];
-                for (int j = 0; j < arrayToReturn[i].Length; j++)
-                {
-                    arrayToReturn[i][j] = i * j;
-                }
-            }
-            return arrayToReturn;
-        }
-
-        public static int[,] BuildMultidimensionalMultiplicationTable(int maxFactor)
-        {
-            int[,] arrayToReturn = new int[maxFactor + 1, maxFactor + 1];
-            for (int i = 0; i < arrayToReturn.GetLength(0); i++)
-            {
-                for (int j = 0; j < arrayToReturn.GetLength(1); j++)
-                {
-                    arrayToReturn[i, j] = i * j;
-                }
-            }
-            return arrayToReturn;
-        }
-
-        public static List<int> GetPowersOf2(int power)
-        {
-            List<int> listToReturn = new List<int>();
-            for (int i = 0; i < power + 1; i++)
-            {
-                listToReturn.Add(i * i);
-            }
-            return listToReturn;
-        }
-
-        //Adding to List
-        public static class MathHelpers
-        {
-            public static List<int> GetPowersOf2(int power)
-            {
-                List<int> listToReturn = new List<int>();
-                for (int i = 0; i < power + 1; i++)
-                {
-                    listToReturn.Add((int)System.Math.Pow(2, i));
-                }
-                foreach (var item in listToReturn)
-                {
-                    System.Console.WriteLine(item);
-                }
-                return listToReturn;
-            }
-        }
-
     }
 }
--- CSharpCollections/Student.cs
+++ CSharpCollections/Student.cs
@@ -13,7 +13,14 @@ namespace CSharpCollections
 
         public int CompareTo(Student that)
         {
+            //Compare names first
             int result = this.Name.CompareTo(that.Name);
+
+            //If names are the same, compare against gradelevel
+            if(result == 0)
+            {
+                result = this.GradeLevel.CompareTo(that.GradeLevel);
+            }
             return result;
         }
     }
