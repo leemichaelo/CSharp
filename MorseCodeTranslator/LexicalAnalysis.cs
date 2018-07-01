55aee6969ae4dec41696a3c5ac8031fd2211fcca
Lee Owen <LeeMichaelO@outlook.com>
2018-07-01 15:35:19 -0700

    Working with dictionaries

---
+++ MorseCodeTranslator/LexicalAnalysis.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+
+namespace MorseCodeTranslator
+{
+    public class LexicalAnalysis
+    {
+        public Dictionary<string, int> WordCount = new Dictionary<string, int>();
+
+        public void AddWord(string word)
+        {
+            bool check = false;
+            foreach (string entry in WordCount.Keys)
+            {
+                if (entry == word)
+                {
+                    check = true;
+                    break;
+                }
+            }
+            if (check)
+            {
+                WordCount[word] += 1;
+            }
+            else
+            {
+                WordCount.Add(word, 1);
+            }
+        }
+
+        public Dictionary<string, int> WordsWithCountGreaterThan(int value)
+        {
+            Dictionary<string, int> DictionaryToReturn = new Dictionary<string, int>();
+
+            foreach (var word in WordCount.Keys)
+            {
+                if (WordCount[word] > value)
+                {
+                    DictionaryToReturn.Add(word, WordCount[word]);
+                }
+            }
+
+            return DictionaryToReturn;
+        }
+    }
+}
---
+++ MorseCodeTranslator/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MorseCodeTranslator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write(": ");
+                string input = Console.ReadLine().Trim();
+
+                if (string.IsNullOrWhiteSpace(input))
+                {
+                    break;
+                }
+
+                string output;
+
+                if (input[0] == '.' || input[0] == '-')
+                {
+                    output = MorseCodeTranslator.ToText(input);
+                }
+                else
+                {
+                    output = MorseCodeTranslator.toMorse(input);
+                }
+
+                Console.WriteLine(output);
+            }
+        }
+    }
+}
